6dd16e14ccc972ee7a325b791016b8bb300a1f5c
95decode <95decode@gmail.com>
2022-10-05 12:04:13 +0900

    Add calc merkle root function

--- index.ts
+++ index.ts
@@ -1,49 +1,16 @@
-import keccak256 from 'keccak256'
-import MerkleTree from './merkletree';
-import { parse } from 'csv';
+import {MerkleTree, csvToLeaves} from './merkletree';
 import * as fs from "fs";
-import BigNumber from 'bignumber.js';
 
-let foo = fs.readFileSync('./sample.csv');
+let sampleCsv = fs.readFileSync('./sample.csv');
 
-parse(foo, {delimiter: ','}, (err, data) => {
-    if(err) {
-        return
-    }
+(async() => {
+    let leaves = await csvToLeaves(sampleCsv);
 
-    for(let i = 0; i < data.length; i++){
-        const idx = i.toString(16);
-        const address = data[i][0].replace('0x', '');
-        const amount = BigNumber(data[i][1]).toString(16);
-        // raw : (idx, address, amount)
-        let raw = zeroPadding(idx) + address + zeroPadding(amount);
-        // leaf : keccak256(raw)
-        let leaf = '0x' + (keccak256(Buffer.from(raw, 'hex')).toString('hex'));
-        console.log(leaf);
-    }
-})
+    let bar = new MerkleTree(leaves);
+    //bar.getleaves();
+    //bar.getRoot();
 
-function zeroPadding(num: string) {
-    const zero32 = '0000000000000000000000000000000000000000000000000000000000000000'
+    bar.getRoot();
+})()
 
-    if(num.length < 64) {
-        return num = (zero32 + num).slice(-64);
-    } else return num;
-}
 
-// idx : 0, account : 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, amount : 10
-// 256bits + 160bits + 256bits
-// 00000000000000000000000000000000000000000000000000000000000000005B38Da6a701c568545dCfcB03FcB875f56beddC4000000000000000000000000000000000000000000000000000000000000000a
-// 0x20469d57a663e836e1c54b49e35f108cc3e8674594eaeefcfef97170b35d0a88
-
-/*
-// result
-// foo : 41b1a0649752af1b28b3dc29a1556eee781e4a4c3a1f7f53f90fa834de098c4d
-// bar : 5dc1c14d56e959d57ab94facd0d6c86740ac46c453bf9107bba1c735d7783c71
-console.log(keccak256('foo').toString('hex'));
-console.log(keccak256(Buffer.from('foo')).toString('hex'));
-*/
-
-const leaves = ['foo', 'var'].map(x => keccak256(Buffer.from(x)).toString('hex'));
-
-const tree = new MerkleTree(leaves);
--- merkletree.ts
+++ merkletree.ts
@@ -1,13 +1,82 @@
+import { parse } from 'csv';
+import BigNumber from 'bignumber.js';
+import keccak256 from 'keccak256'
+
 export class MerkleTree {
-    constructor(leaves: any[]) {
-        //console.log(leaves);
+    private leaves: string[] = [];
+    private layer: Array<string[]> = [];
+
+    constructor(_leaves: string[]) {
+        this.leaves = _leaves;
+
+        this.layer.push(this.leaves)
     }
 
-    getRoot() {
-
+    public getleaves() {
+        console.log(this.leaves);
     }
 
+    public getRoot() {
+        if(!this.leaves.length) return;
 
+        this.recur(this.leaves);
+
+        console.log(this.layer);
+    }
+
+    private recur(data: string[]) {
+        if(!data.length) return;
+        
+        let nextLayer: string[] = [];
+
+        for(let i=0; i<data.length; i++) {
+            if(i+1 == data.length) {
+                nextLayer.push(data[i]);
+                break;
+            }
+
+            // i, i+1 연산 함
+            let raw = [data[i], data[i+1]];
+            raw.sort()
+            nextLayer.push(keccak256(Buffer.from(raw[0]+raw[1], 'hex')).toString('hex'));
+            i++
+        }
+        this.layer.push(nextLayer);
+
+        if(nextLayer.length != 1) {
+            this.recur(nextLayer)
+        } else {
+            console.log(nextLayer);
+        };
+    }
+}
+
+export function csvToLeaves(csvData: Buffer): Promise<string[]> {
+    return new Promise(resolve => {
+        parse(csvData, {delimiter: ','}, (err, data) => {
+            if(err) return;
+            let leaves: string[] = []
+            for(let i = 0; i < data.length; i++){
+                const idx = i.toString(16);
+                const address = data[i][0].replace('0x', '');
+                const amount = BigNumber(data[i][1]).toString(16);
+                // 256bits + 160bits + 256bits
+                // raw : (idx, address, amount)
+                let raw = zeroPadding(idx, 64) + address + zeroPadding(amount, 64);
+                // leaf : keccak256(raw)
+                let leaf = keccak256(Buffer.from(raw, 'hex')).toString('hex');
+                leaves.push(leaf);
+            }
+            resolve(leaves);
+        });
+    })
+}
+
+export function zeroPadding(num: string, len: number): string {
+    const zero32 = '0000000000000000000000000000000000000000000000000000000000000000'
+    if(num.length < len) {
+        return num = (zero32 + num).slice(-len);
+    } else return num;
 }
 
 export default MerkleTree
